215d995a7dbf33321636028813f9e556385a2a0c
Vinay Barod <vbarod@mobiquityinc.com>
2026-04-08 05:01:38 +0530

    Java Syntax Essential Code added

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{ //Access modifier default private protected and public 
+    public static void main(String args[]){// static --> Access via class name
+        System.out.println("Hello");// System is a predefined class which is present in java.lang package
+        //out--> Out is an static object of PrintStream class 
+        //println is a method of printStream
+    }
+}
+
+// class --> User defined Data type
+
+// javac Main.java --> Compiler --> All At once
+//Interperter --> Line by line conversion
---
+++ MethodDemo.java
@@ -0,0 +1,12 @@
+class Addition{
+    public static int add(int a,int b){
+        return a+b;
+    }
+    static public void printMethod(String message){
+        System.out.print(message);
+    }
+     static public void main(String args[]){
+        System.out.print(add(1, 2));
+    }
+
+}
